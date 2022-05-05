859d0c7720ccd7b219b3b08f9bfee6a31b658c22
Yauhen Baranau <yauhen.baranau1@gmail.com>
2022-05-05 23:24:16 +0300

    fixed Votes task

--- Votes/server/server.js
+++ Votes/server/server.js
@@ -1,42 +1,43 @@
 const express = require("express");
 
+const fs = require("fs");
+const path = require("path");
+
 const webserver = express();
 
-webserver.use(express.urlencoded({extended:true}));
+webserver.use(express.urlencoded({ extended: true }));
 webserver.use(express.json());
 
 const port = 80;
 
-const variants = [
-   {code: 1, text: 'ВИСКИ', count:0},
-   {code: 2, text: 'ВОДКА', count:0},
-   {code: 3, text: 'СОК', count:0},
-];
-
 webserver.get("/index", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-  });
+  res.sendFile(__dirname + "/index.html");
+});
 
-  webserver.get("/variants", (req, res) => {
-    res.send(variants); 
-  });
-  
-  webserver.post("/vote", (req, res) => {
+webserver.get("/variants", (req, res) => {
+  const variants = fs.readFileSync("variants.json", "utf8");
+  res.send(variants);
+});
 
-    if (!req.body) {
-        return res.sendStatus(400)
+webserver.post("/vote", (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
 
-    }
+  const newStat = JSON.parse(fs.readFileSync("stat.json", "utf8")); // получили json в кодировек utf8, распарсили
+  newStat.find((v) => v.code === req.body.code).count++; // что-то сделали
+  const stat = path.join(__dirname, "stat.json"); // получили полный путь
+  const logFd = fs.openSync(stat, "w"); // открыли в файловой системе по полученному пути
+  fs.writeSync(logFd, JSON.stringify(newStat)); // переписали
+  fs.closeSync(logFd); // закрыли в файловой системе
+  res.sendStatus(200);
+});
 
-    variants.find(v => v.code === req.body.code).count ++;
-    res.sendStatus(200)
-  }); 
-  
-  webserver.get("/stat", (req, res) => {
-    res.sendFile(__dirname + "/stat.html");
-  });
-
+webserver.get("/stat", (req, res) => {
+  const stat = fs.readFileSync("stat.json", "utf8");
+  res.send(stat);
+});
 
 webserver.listen(port, () => {
-    console.log("web server running on port " + port);
-  });
+  console.log("web server running on port " + port);
+});
