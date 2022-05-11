bb03fa755e1a2bb6509c0f5702505ebf00755080
Yauhen Baranau <yauhen.baranau1@gmail.com>
2022-05-11 17:50:47 +0300

    3595

--- Votes/server/server.js
+++ Votes/server/server.js
@@ -10,9 +10,11 @@ webserver.use(express.json());
 
 const port = 80;
 
-webserver.get("/index", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// webserver.get("/index", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+webserver.use("/index", express.static(path.resolve(__dirname)));
 
 webserver.get("/variants", (req, res) => {
   const variants = fs.readFileSync("variants.json", "utf8");
@@ -35,9 +37,40 @@ webserver.post("/vote", (req, res) => {
 
 webserver.get("/stat", (req, res) => {
   const stat = fs.readFileSync("stat.json", "utf8");
-  res.send(stat);
+  res.setHeader("Cache-Control", "public, max-age=0");
+  res.setHeader("Content-Type", req.headers.accept);
+  res.send(convertDocumentToMime(req.headers.accept, stat));
 });
 
 webserver.listen(port, () => {
   console.log("web server running on port " + port);
 });
+
+function convertDocumentToMime(mime, data) {
+  let dataCode = JSON.parse(data);
+
+  switch (mime) {
+    case "application/json":
+      return data;
+
+    case "text/html":
+      // let dataCode = JSON.parse(data);
+      let strHTML = "";
+      dataCode.forEach((element) => {
+        strHTML += `<div><span>id</span><span>${element.code}</span> <span>count</span><span>${element.count}</span></div> <br/>`;
+      });
+
+      return strHTML;
+
+    case "application/xml":
+      let strXML = `<root>`;
+      dataCode.forEach((element) => {
+        strXML += `<code>${element.code}</code>
+        <count>${element.count}</count>`;
+      });
+
+      return strXML + "</root>";
+
+      default:  return console.log('unknown MIMEtype');
+  }
+}
